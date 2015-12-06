5f053c59f0f6e6e356c72ebed97beb907644bb77
dyngar <dyngar@dyngar.local>
2015-12-06 00:37:17 -0500

    laid out plugin

---
+++ debian.sh
@@ -0,0 +1,75 @@
+#! /bin/sh
+# Configure your paths and filenames
+SOURCEBINPATH=.
+SOURCEDIR=netmonitor/
+SOURCEBIN=netmonitor/boddy.lua
+IFACEBIN=nmap-auto-wrapper
+CONNBIN=nmap-auto-scanner
+SOURCEDOC=README.md
+DEBFOLDER=svirfneblin-netmonitor-widget
+
+DEBVERSION=$(date +%Y%m%d)
+
+cd $DEBFOLDER
+
+git pull origin master
+
+DEBFOLDERNAME="../$DEBFOLDER-$DEBVERSION"
+DEBPACKAGENAME=$DEBFOLDER\_$DEBVERSION
+
+rm -rf $DEBFOLDERNAME
+# Create your scripts source dir
+mkdir $DEBFOLDERNAME
+
+# Copy your script to the source dir
+cp -R $SOURCEBINPATH/ $DEBFOLDERNAME/
+cd $DEBFOLDERNAME
+
+pwd
+
+# Create the packaging skeleton (debian/*)
+dh_make -s --indep --createorig 
+
+mkdir -p debian/tmp/usr
+cp -R usr debian/tmp/usr
+
+# Remove make calls
+grep -v makefile debian/rules > debian/rules.new 
+mv debian/rules.new debian/rules 
+
+# debian/install must contain the list of scripts to install 
+# as well as the target directory
+echo etc/xdg/svirfneblin/rc.lua.pech.example etc/xdg/svirfneblin >> debian/install
+echo etc/xdg/svirfneblin/$SOURCEBIN etc/xdg/svirfneblin/$SOURCEDIR >> debian/install
+echo usr/bin/$IFACEBIN usr/bin >> debian/install
+echo usr/bin/$CONNBIN usr/bin >> debian/install
+cp $SOURCEDOC usr/share/doc/$DEBFOLDER/$SOURCEDOC
+echo usr/share/doc/$DEBFOLDER/$SOURCEDOC usr/share/doc/$DEBFOLDER >> debian/install
+
+echo "Source: $DEBFOLDER
+Section: unknown
+Priority: optional
+Maintainer: cmotc <cmotc@openmailbox.org>
+Build-Depends: debhelper (>= 9)
+Standards-Version: 3.9.5
+Homepage: https://www.github.com/cmotc/svirfneblin-netmonitor-widget
+#Vcs-Git: git@github.com:cmotc/svirfneblin-netmonitor-widget
+#Vcs-Browser: https://www.github.com/cmotc/svirfneblin-netmonitor-widget
+
+Package: $DEBFOLDER
+Architecture: all
+Depends: lightdm, lightdm-gtk-greeter, awesome (>= 3.4), svirfneblin-panel \${misc:Depends}
+Description: A network monitoring widget for awesomewm
+" > debian/control
+
+#echo "gsettings set org.gnome.desktop.session session-name awesome-gnome
+#dconf write /org/gnome/settings-daemon/plugins/cursor/active false
+#gconftool-2 --type bool --set /apps/gnome_settings_daemon/plugins/background/active false
+#" > debian/postinst
+# Remove the example files
+rm debian/*.ex
+rm debian/*.EX
+
+# Build the package.
+# You  will get a lot of warnings and ../somescripts_0.1-1_i386.deb
+debuild -us -uc >> ../log
